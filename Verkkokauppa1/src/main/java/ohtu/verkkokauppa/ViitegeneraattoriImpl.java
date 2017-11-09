696e30f6c1fd49f0f77a9dec2f9d6901aa316e54
Leevi Lehtonen <leevi.k.lehtonen@helsinki.fi>
2017-11-09 15:46:52 +0200

    annotaatio valmis

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/ViitegeneraattoriImpl.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/ViitegeneraattoriImpl.java
@@ -1,9 +1,13 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ViitegeneraattoriImpl implements Viitegeneraattori {
     
     private int seuraava;
-    
+
+
     public ViitegeneraattoriImpl(){
         seuraava = 1;    
     }
